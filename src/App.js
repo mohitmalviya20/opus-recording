e679cbc481ee3604022085a36eb37c3d88e7dbde
mohitmalviya <you@example.com>
2021-01-19 18:18:55 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data=[]
+  var blobs=new Blob(data)
+  let device = navigator.mediaDevices.getUserMedia({audio:true})
+  let recorder;
+  device.then(stream=>{
+    recorder = new MediaRecorder(stream)
+    recorder.ondataavailable = e => {
+      data.push(e.data)
+      if(recorder.state=='inactive'){
+        let blobData= new Blob(data,{type:"audio/ogg" })
+        document.getElementById('audio').innerHTML= `<source src="${URL.createObjectURL(blobData)}" type="audio/ogg"/>`
+        console.log(blobData)
+
+      }
+
+
+    }
+ 
+
+  })
+  console.log(blobs)
+  const startRec=()=>{
+    recorder.start();
+  }
+  const stopRec=()=>{
+    recorder.stop()
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Opus audio recorder</h2>
+     <audio id="audio" controls>
+
+       
+
+       
+     </audio>
+     <div>     
+      <button onClick={startRec}>start</button>
+      <button onClick={stopRec}>stop</button>
+      </div>
+
     </div>
   );
 }
